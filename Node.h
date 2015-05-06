d63c886957b1285e40537792aac7e405b2fb700f
Deidara19 <deoxis_48@hotmail.com>
2015-05-05 20:09:29 -0500

    Create Node.h

---
+++ Node.h
@@ -0,0 +1,19 @@
+#ifndef NODE_H_INCLUDED
+#define NODE_H_INCLUDED
+
+#include<iostream>
+using namespace std;
+
+class Node
+{
+	public:
+		int C, H;                 // C = Cost, H = Heuristic
+		bool visited;             // Marks if the node is visited (1 for Visited & 0 for !Visited).
+		Node **adyacent;          /* Adyacent Nodes of the current node, 
+							      each node contains 4 directional pads
+								  (North, East, South and West) */
+		Node();
+		~Node();
+};
+
+#endif
