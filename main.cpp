cc139b229123ca0cffc2ef01fa68c02f15582d6f
Deidara19 <deoxis_48@hotmail.com>
2015-05-05 21:44:29 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Maze.h"
+#include <stdlib.h>
+
+int main()
+{
+	Maze M;
+
+	cout << "\tEXAMEN PARCIAL #2\nALGORITMOS Y ESTRUCTURAS DE DATOS \n  FECHA DE ENTREGA: 07/05/15\n\t  " << endl;
+	cout << "\tA* ALGORITHM" << endl << endl;
+
+	M.printMaze();
+
+	cin.get();
+	return 0;
+}
