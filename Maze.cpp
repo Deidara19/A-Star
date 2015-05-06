cc9aab50229ca57f92fc2abfd7397c84ed54a0fb
Deidara19 <deoxis_48@hotmail.com>
2015-05-06 01:50:06 -0500

    Commend me

--- Maze.cpp
+++ Maze.cpp
@@ -6,7 +6,7 @@ Maze::Maze()
 	rows = 0;
 	cols = 0;
 	costo = 0;
-	readInput(); 
+	readInput();
 	//auxx = start.x;
 	//auxy = start.y;
 }
@@ -14,7 +14,7 @@ Maze::Maze()
 Maze::~Maze()
 {
 	for (int i = 0; i < rows; i++)
-			delete[] M[i];
+		delete[] M[i];
 	delete[] M;
 }
 
@@ -40,8 +40,11 @@ void Maze::readInput()
 	while (input >> start.x >> coma >> start.y >> final.x >> coma >> final.y)
 	{
 		/* AQUÍ SE MANDA A LLAMAR LA FUNCIÓN */
+		setAdjacencies();
+		heuristic();
+		//astar(start.x, start.y);
 	}
-		
+
 	input.close();
 }
 
@@ -50,42 +53,50 @@ void Maze::setAdjacencies()
 	for (int i = 0; i < rows; i++)
 		for (int j = 0; j < cols; j++)
 		{
-			if (i - 1 > 0)
-			{
-				if (M[i - 1][j].C != -1)
-					M[i][j].adyacent[0] = &M[i - 1][j];
+		if (i - 1 > 0)
+		{
+			if (M[i - 1][j].C != -1)
+				M[i][j].adyacent[0] = &M[i - 1][j];
 
-				else
-					M[i][j].adyacent[0] = NULL;
-			}
-			
-			if (j + 1 < cols)
-			{
-				if (M[i][j + 1].C != -1)
-					M[i][j].adyacent[1] = &M[i][j + 1];
+			else
+				M[i][j].adyacent[0] = NULL;
+		}
+		else
+			M[i][j].adyacent[0] = NULL;
 
-				else
-					M[i][j].adyacent[1] = NULL;
-			}
+		if (j + 1 < cols)
+		{
+			if (M[i][j + 1].C != -1)
+				M[i][j].adyacent[1] = &M[i][j + 1];
 
-			if (i + 1 < rows)
-			{
-				if (M[i + 1][j].C != -1)
-					M[i][j].adyacent[2] = &M[i + 1][j];
+			else
+				M[i][j].adyacent[1] = NULL;
+		}
+		else
+			M[i][j].adyacent[1] = NULL;
 
-				else
-					M[i][j].adyacent[2] = NULL;
-			}
-			
-			if (j - 1 > 0)
-			{
-				if (M[i][j - 1].C != -1)
-					M[i][j].adyacent[3] = &M[i][j - 1];
+		if (i + 1 < rows)
+		{
+			if (M[i + 1][j].C != -1)
+				M[i][j].adyacent[2] = &M[i + 1][j];
 
-				else
-					M[i][j].adyacent[3] = NULL;
-			}
-		}	
+			else
+				M[i][j].adyacent[2] = NULL;
+		}
+		else
+			M[i][j].adyacent[2] = NULL;
+
+		if (j - 1 > 0)
+		{
+			if (M[i][j - 1].C != -1)
+				M[i][j].adyacent[3] = &M[i][j - 1];
+
+			else
+				M[i][j].adyacent[3] = NULL;
+		}
+		else
+			M[i][j].adyacent[3] = NULL;
+		}
 }
 
 void Maze::printMaze()
@@ -93,24 +104,35 @@ void Maze::printMaze()
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < cols; j++)
-			cout << M[i][j].C << ' ';
+		{
+			cout << "(" << i << ", " << j << ") COSTOS ADY:" << endl;
+			for (int k = 0; k < 4; k++)
+			{
+				if (M[i][j].adyacent[k] != NULL)
+					cout << M[i][j].adyacent[k]->C << " ";
+
+				else
+					cout << "NULL ";
+			}
+			cout << endl;
+		}
 		cout << endl;
 	}
 }
 
 int Maze::abs(int x)
 {
-    if(x<0)
-        x*=-1;
-    return x;
+	if (x<0)
+		x *= -1;
+	return x;
 }
 
 void Maze::heuristic()
 {
-    //cout<<final.x<<endl<<final.y<<endl;
-    for(int i=0;i<rows;i++)
-        for(int j=0;j<cols;j++)
-             M[i][j].H = abs(final.x - i) + abs(final.y - j);
+	//cout<<final.x<<endl<<final.y<<endl;
+	for (int i = 0; i<rows; i++)
+		for (int j = 0; j<cols; j++)
+			M[i][j].H = abs(final.x - i) + abs(final.y - j);
 
 }
 
@@ -118,16 +140,17 @@ void Maze::astar(int auxx, int auxy)
 {
 	if (auxx == final.x && auxy == final.y) return;
 	Node *aux;
+	aux = NULL;
 	int csm = 0;
 	int costo_aux = 10000000;
-	
+
 	for (int i = 0; i < 4; i++) // es 4 porque horacio se le da la puta gana, hdp, report pls, fucking peruvian
 	{
 		if (M[auxx][auxy].adyacent[i] != NULL)
 		{
 			if (M[auxx][auxy].adyacent[i]->C + M[auxx][auxy].adyacent[i]->H < costo_aux)
 			{
-				aux = M[auxx][auxy].adyacent[i]; 
+				aux = M[auxx][auxy].adyacent[i];
 				csm = i;
 				costo_aux = M[auxx][auxy].adyacent[i]->C + M[auxx][auxy].adyacent[i]->H;
 			}
@@ -135,14 +158,16 @@ void Maze::astar(int auxx, int auxy)
 	}
 
 	costo += aux->C;
-	
+
+	cout << auxx << ", " << auxy << endl;
+
 	if (csm == 0)
 		auxy -= 1;
 	else if (csm == 1)
 		auxx += 1;
 	else if (csm == 2)
 		auxy += 1;
-	else if(csm == 3)
+	else if (csm == 3)
 		auxx -= 1;
 
 	aux->visited = true;
