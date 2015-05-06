34b2bcf3162ebd524a077a7411187b646d409ef8
Deidara19 <deoxis_48@hotmail.com>
2015-05-05 22:15:56 -0500

    Update Maze.cpp

--- Maze.cpp
+++ Maze.cpp
@@ -17,5 +17,80 @@ Maze::~Maze()
 
 void Maze::readInput()
 {
+	char coma;
 
+	ifstream input("Examen2-1.txt");
+
+	input >> rows;
+	input >> cols;
+
+	M = new Node*[rows];
+	for (int i = 0; i < rows; i++)
+		M[i] = new Node[cols];
+
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < cols; j++)
+			input >> M[i][j].C;
+
+	setAdjacencies();
+
+	while (input >> start.x >> coma >> start.y >> final.x >> coma >> final.y)
+	{
+		/* AQUÍ SE MANDA A LLAMAR LA FUNCIÓN */
+	}
+		
+	input.close();
+}
+
+void Maze::setAdjacencies()
+{
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < cols; j++)
+		{
+			if (i - 1 > 0)
+			{
+				if (M[i - 1][j].C != -1)
+					M[i][j].adyacent[0] = &M[i - 1][j];
+
+				else
+					M[i][j].adyacent[0] = NULL;
+			}
+			
+			if (j + 1 < cols)
+			{
+				if (M[i][j + 1].C != -1)
+					M[i][j].adyacent[1] = &M[i][j + 1];
+
+				else
+					M[i][j].adyacent[1] = NULL;
+			}
+
+			if (i + 1 < rows)
+			{
+				if (M[i + 1][j].C != -1)
+					M[i][j].adyacent[2] = &M[i + 1][j];
+
+				else
+					M[i][j].adyacent[2] = NULL;
+			}
+			
+			if (j - 1 > 0)
+			{
+				if (M[i][j - 1].C != -1)
+					M[i][j].adyacent[3] = &M[i][j - 1];
+
+				else
+					M[i][j].adyacent[3] = NULL;
+			}
+		}	
+}
+
+void Maze::printMaze()
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+			cout << M[i][j].C << ' ';
+		cout << endl;
+	}
 }
